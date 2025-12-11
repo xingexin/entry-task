0bdecffa6fa40e767565e608245f12ad22e0a8fd
ZhuoChuyao <Chuyao.Zhuo@shopee.com>
2025-12-11 11:38:17 +0800

    first for init :确定了初始架构设计

---
+++ go.mod
@@ -0,0 +1,3 @@
+module entry-task
+
+go 1.25.5
---
+++ httpserver/cmd/httpserver/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for i := 0; i < 100; i++ {
+		fmt.Println("client ", i)
+		time.Sleep(1 * time.Second)
+	}
+}
